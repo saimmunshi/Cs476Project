5ad19249c343915d7ca91b80eb317cd6fa97b1ec
Saim Munshi <saimmunshi@gmail.com>
2026-03-07 07:05:23 -0600

    task are now added to the course description display

--- teachers/features/tasks/static/js/Create_Task.js
+++ teachers/features/tasks/static/js/Create_Task.js
@@ -9,26 +9,32 @@ let taskMapContent = document.getElementById("task-map-content-div");
 
 
 /*___________________________________________________________________________________________________________________________________*/
-/*Added by Saim Munshi: Course title*/
+
 const courseTitle = document.getElementById("courseSelect");
 const courseTitleContainer = document.getElementById("course-title-container");
 const courseTitleNode = document.getElementById("course-node-title");
 /*___________________________________________________________________________________________________________________________________*/
 
 const courseDescriptionDisplay = document.getElementById("course-description-display");
+/*Added by Saim Munshi: Add Button variable*/
 
-
-const updateButton = document.getElementById("Update-Course");
+const taskTypeSelect = document.getElementById("Task-Type");
+const duetaskDateInput = document.getElementById("due_date");
+const starttaskDateInput = document.getElementById("start_date");
 const addButton = document.getElementById("Add-Task");
+const courseTask = document.getElementById("title");
+/*___________________________________________________________________________________________________________________________________*/
+const updateButton = document.getElementById("Update-Course");
+
 const deleteButton = document.getElementById("Delete-Task");
 
-const courseTask = document.getElementById("title");
+
 const courseDescription = document.getElementById("Course-Description");
 
 
 const descriptionNodeDisplay = document.getElementById("roadmap-body-div");
-const taskDateInput = document.getElementById("Task-Date");
-const taskTypeSelect = document.getElementById("Task-Type");
+
+
 
 /*___________________________________________________________________________________________________________________________________*/
 
@@ -72,3 +78,98 @@ courseTitle.addEventListener('change', function () {
 
 });
 
+  addButton.addEventListener('click', e => {
+        e.preventDefault();
+
+        /*Added by Saim Munshi: Retreiev taskValue from */
+        const taskValue = courseTask.value.trim();
+
+         /*Added by Saim Munshi: if taskvalue does not exist*/
+        if (!taskValue) return;
+         /* Added By Saim Munshi: this code deal with creating and adding task check box list  */
+
+        const taskIdCheckBox = `task-${counter}`;
+        counter++;
+        const checkboxDiv = document.createElement("div");
+        /* Added By Saim Munshi: creating input checkbox with id equal taskIdCheckBox */
+        checkboxDiv.innerHTML = `
+            <input class="form-check-input me-2"
+                type="checkbox"
+                id="${taskIdCheckBox}"
+                data-status="not_started">
+     
+            
+            <label class="form-check-label fw-bold" for="${taskIdCheckBox}">
+                ${taskValue}
+            </label>
+   
+            <span class="badge bg-secondary ms-2 status-badge mb-1">
+                Not Started
+            </span>
+
+            <div class="small text-muted mb-3">
+                Type: ${taskTypeSelect.value} |
+                Start Date: ${starttaskDateInput.value ? starttaskDateInput.value : "No date"}
+                Due: ${duetaskDateInput.value ? duetaskDateInput.value : "No date"}
+            </div>
+        `;
+        /* ---------------------------------------------------------------------*/
+
+        /* Added By Saim Munshi:  this logic to set the status for the tasks*/
+       const checkboxElement = checkboxDiv.querySelector("input");
+       const statusBadge = checkboxDiv.querySelector(".status-badge");
+
+        /* Added By Saim Munshi:  this logic to set the status for the tasks*/
+        checkboxElement.addEventListener("change", function () {
+        let currentStatus = this.getAttribute("data-status");
+
+        if (currentStatus === "not_started") {
+            this.setAttribute("data-status", "in_progress");
+            checkboxDiv.className = "form-check mb-2 text-warning";
+            statusBadge.className = "badge bg-warning ms-2 status-badge";
+            statusBadge.textContent = "In Progress";
+            this.checked = false; 
+        }
+        else if (currentStatus === "in_progress") {
+            this.setAttribute("data-status", "completed");
+            checkboxDiv.className = "form-check mb-2 text-success";
+            statusBadge.className = "badge bg-success ms-2 status-badge";
+            statusBadge.textContent = "Completed";
+            this.checked = true;
+        }
+        else {
+            this.setAttribute("data-status", "not_started");
+            checkboxDiv.className = "form-check mb-2 text-secondary";
+            statusBadge.className = "badge bg-secondary ms-2 status-badge";
+            statusBadge.textContent = "Not Started";
+            this.checked = false;
+        }
+    });
+        /*--------------------------------------------------------------------------*/
+
+        /* Added By Saim Munshi: this code stores the date when the course was created */
+        courseDescriptionDisplay.parentNode.appendChild(checkboxDiv);
+        const taskType = taskTypeSelect.value;
+        const dueDate = duetaskDateInput.value;
+        const startDate = starttaskDateInput.value;
+        /*------------------------------------------------------------------------------*/
+
+        /*  Added By Saim Munshi: Bases on the type this determines the icon type based on type when type is selected appropriate bootstrap class  is added*/
+        /* This is Bootstrap Icon Logic */
+        let iconClass = "bi-lock-fill";
+        let nodeColor = "bg-primary";
+        if ((taskType === "Quiz")) {
+            iconClass = "bi-question-circle-fill";
+            nodeColor = "bg-warning";
+        }
+        if (taskType === "Project") {
+            iconClass = "bi-clipboard-check-fill";
+            nodeColor = "bg-danger";
+        }
+        if (taskType === "Assignment") {
+            iconClass = "bi-journal-text";
+            nodeColor = "bg-info";
+        }
+        /*-----------------------------------------------------------------------------------------------------------*/
+         courseTask.value = "";
+  })
