55eaed805eb20b7c9dddfa7ef8f7d19e4c8fd2e5
Matthew Krusk <mkrusk1@hotmail.com>
2026-03-28 14:02:54 -0600

    removed hide sidebar now that not needed

--- core/settings.py
+++ core/settings.py
@@ -221,9 +221,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'teachers/BaseTeacher/static'),
 ]
 
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
-
 from django.core.servers.basehttp import WSGIServer
